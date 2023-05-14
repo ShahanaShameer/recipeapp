bba8fd2bb69f1635cb7405079bb02d4061b57840
Arjun <92151192+arjunjayakumar@users.noreply.github.com>
2023-05-14 11:03:21 +0530

    Remove console statements

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
-import {map} from 'rxjs/operators'; 
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -18,7 +24,7 @@ export class RecipeEditComponent {
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService,
-    private fb:FormBuilder
+    private fb: FormBuilder
   ) {}
   ngOnInit() {
     // this.recipeForm = this._formBuilder.group({
@@ -27,104 +33,84 @@ export class RecipeEditComponent {
     //   description:[ "", Validators.required ]
 
     // });
-    this.route.params
-    .subscribe((params: Params) => {
+    this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
-      
+
       this.initForm();
     });
   }
   onSubmit() {
-   
-    
-  
-        if(this.editMode){
-      this.recipeService.updateRecipe(this.id,this.recipeForm.value)
-     }
-     else{
-      this.recipeService.addRecipe(this.recipeForm.value)
-     }
+    if (this.editMode) {
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
   }
-  onAddIngredient(){
+  onAddIngredient() {
     (<FormArray>this.recipeForm.get('recipeIngredients')).push(
       new FormGroup({
-        'name':new FormControl(null,Validators.required),
-        'Amount':new FormControl(null,[
+        name: new FormControl(null, Validators.required),
+        Amount: new FormControl(null, [
           Validators.required,
-          Validators.pattern(/^[1-9]+[0-9]*$/)
-        ])
+          Validators.pattern(/^[1-9]+[0-9]*$/),
+        ]),
       })
     );
   }
- 
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
-    let recipeIngredients =new FormArray([]);
-    this.recipeForm=new FormGroup({
-      'name':new FormControl(recipeName),
-      'imagePath':new FormControl(recipeImagePath),
-      'description':new FormControl(recipeDescription)
+    let recipeIngredients = new FormArray([]);
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription),
     });
-    
+
     const recipe = this.recipeService.getRecipe(this.id);
-   
+
     if (this.editMode) {
-      
-    const recipe = this.recipeService.getRecipe(this.id);
+      const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
-      recipeDescription=recipe.description
-      
+      recipeDescription = recipe.description;
     }
-    this.recipeForm=new FormGroup({
-      'name':new FormControl(recipeName),
-      'imagePath':new FormControl(recipeImagePath),
-      'description':new FormControl(recipeDescription),
-      'recipeIngredients':recipeIngredients
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription),
+      recipeIngredients: recipeIngredients,
     });
-    
-    console.log(recipe);
-    
-      if (recipe['ingredients']) {
-        
-        for (let ingredient of recipe.ingredients) {
-          const recipeIngredients=this.recipeForm.get('recipeIngredients') as FormArray
-          recipeIngredients.push(
-       new  FormGroup({
-              'name': new FormControl(ingredient.name,Validators.required),
-              'amount': new FormControl(ingredient.amount,
-                [
-                  Validators.required,
-                  Validators.pattern(/^[1-9]+[0-9]*$/)
-                ])
-              
-            })
-            
-            );
 
-          
-            
-        
-        }
-      
+    if (recipe['ingredients']) {
+      for (let ingredient of recipe.ingredients) {
+        const recipeIngredients = this.recipeForm.get(
+          'recipeIngredients'
+        ) as FormArray;
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(ingredient.name, Validators.required),
+            amount: new FormControl(ingredient.amount, [
+              Validators.required,
+              Validators.pattern(/^[1-9]+[0-9]*$/),
+            ]),
+          })
+        );
       }
-    
-
-this.recipeForm=new FormGroup({
-  'name':new FormControl(recipeName,Validators.required),
-  'imagePath':new FormControl(recipeImagePath,Validators.required),
-  'description':new FormControl(recipeDescription,Validators.required),
-  'recipeIngredients':recipeIngredients
-});
-  
     }
+
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName, Validators.required),
+      imagePath: new FormControl(recipeImagePath, Validators.required),
+      description: new FormControl(recipeDescription, Validators.required),
+      recipeIngredients: recipeIngredients,
+    });
+  }
   get controls() {
     // a getter!
     return (<FormArray>this.recipeForm.get('recipeIngredients')).controls;
-  } 
+  }
 }
-
-
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,53 +1,44 @@
-import { EventEmitter, Injectable } from "@angular/core";
-import { Recipe } from "./recipe.model";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
-import { Subject } from "rxjs";
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 @Injectable()
-export class RecipeService{
-  recipesChanged = new Subject<Recipe[]>(); 
-    
-  private  recipes:Recipe[] =[
-        new Recipe(
-            'Tasty Schnitzel',
-            'A super-tasty Schnitzel - just awesome!',
-            'https://www.thespruceeats.com/thmb/BS5BdyQsGh5qQyRiuZMannmcxoY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SES-wiener-schnitzel-recipe-1447089-hero-02-18827aac4cbd4aec926350d4f9778e70.jpg',
-            [
-                new Ingredient('Meat',1),
-                new Ingredient('Frech Fries',20)
-            ]),
-        new Recipe(
-            'Big Fat Burger',
-            'What else you need to say?',
-            'https://www.chicken.ca/wp-content/uploads/2013/05/Moist-Chicken-Burgers.jpg',
-            [
-              new Ingredient('Buns',2),
-              new Ingredient ('Meat',1) 
-            ])
-      ];
-      constructor(private slService:ShoppingListService){
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-      }
-      getRecipes(){
-        return this.recipes.slice();
-      }
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://www.thespruceeats.com/thmb/BS5BdyQsGh5qQyRiuZMannmcxoY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/SES-wiener-schnitzel-recipe-1447089-hero-02-18827aac4cbd4aec926350d4f9778e70.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Frech Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://www.chicken.ca/wp-content/uploads/2013/05/Moist-Chicken-Burgers.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+  constructor(private slService: ShoppingListService) {}
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
-      getRecipe(index:number){
-        console.log(index,this.recipes,this.recipes[index]);
-        
-        return this.recipes[index];
-      }
-      
-    addIngredientsToShoppingList(recipeIngredients : Ingredient[]){
-        this.slService.addIngredients(recipeIngredients)
-    }  
-    addRecipe(recipe: Recipe){
-      this.recipes.push(recipe);
-      this.recipesChanged.next(this.recipes.slice());
-    }
-    updateRecipe(index:number, newRecipe:Recipe){
-      this.recipes[index]= newRecipe;
-      this.recipesChanged.next(this.recipes.slice());
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    }
+  addIngredientsToShoppingList(recipeIngredients: Ingredient[]) {
+    this.slService.addIngredients(recipeIngredients);
+  }
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
